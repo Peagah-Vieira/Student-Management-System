4a4e0902290aa32b1d0baf5f27943091adca22ad
Peagah <peagahvieira2003@gmail.com>
2022-09-21 00:17:15 -0300

    #

--- backend/student_data.php
+++ backend/student_data.php
@@ -1,16 +1,4 @@
 <?php
-$url = $_SERVER['PHP_SELF'];
-$maximo = 10; 
-$pagina = isset($_GET['pagina']) ? ($_GET['pagina']) : '1'; 
-$inicio = $pagina - 1; 
-$inicio = $maximo * $inicio;
-$previous = $pagina - 1;        
-$next = $pagina + 1;
-
-echo "<script>console.log('$pagina')</script>";
-echo "<script>console.log('$inicio')</script>";
-echo "<script>console.log('$url')</script>";
-
-$stm = $conn->query("SELECT StudentName,StudentID,StudentEmail,StudentClass,StudentGender,AdmissionDate FROM student_sms LIMIT $inicio, $maximo");
+$stm = $conn->query("SELECT StudentName,StudentID,StudentEmail,StudentClass,StudentGender,AdmissionDate FROM student_sms");
 $db_student = $stm->fetchAll(PDO::FETCH_ASSOC);
 ?>
