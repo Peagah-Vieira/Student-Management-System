b852ba03f09365b796afbc5282eee063768a1859
Peagah <peagahvieira2003@gmail.com>
2022-09-23 14:18:36 -0300

    Register Class with Error Management

--- backend/class_register.php
+++ backend/class_register.php
@@ -1,14 +1,24 @@
 <?php
 if(isset($_POST['class'],$_POST['opening_Date'],$_POST['conclusion_Date'])){
-    $class = filter_input(INPUT_POST,"class");
-    $opening_Date = filter_input(INPUT_POST,"opening_Date");
-    $conclusion_Date = filter_input(INPUT_POST,"conclusion_Date");
-    $status = 1;
-    $stm = $conn->prepare('INSERT INTO class_sms(Class,Opening_Date,Conclusion_Date,status) VALUES (:Class,:Opening_Date,:Conclusion_Date,:status)');
-    $stm->bindParam(':Class', $class);
-    $stm->bindParam(':Opening_Date', $opening_Date);
-    $stm->bindParam(':Conclusion_Date', $conclusion_Date);
-    $stm->bindParam(':status', $status);
-    $stm->execute();
+    $verification = $_POST['class'];
+    $query = $conn->query("SELECT Class FROM class_sms WHERE class = '$verification'");
+    $db_verification = $query->fetch(PDO::FETCH_ASSOC);
+
+    if($verification == $db_verification['Class']){
+        echo "<script>console.log('Class already registered')</script>";
+    }
+    else{
+        $class = filter_input(INPUT_POST,"class");
+        $opening_Date = filter_input(INPUT_POST,"opening_Date");
+        $conclusion_Date = filter_input(INPUT_POST,"conclusion_Date");
+        $status = 1;
+        $stm = $conn->prepare('INSERT INTO class_sms(Class,Opening_Date,Conclusion_Date,status) VALUES (:Class,:Opening_Date,:Conclusion_Date,:status)');
+        $stm->bindParam(':Class', $class);
+        $stm->bindParam(':Opening_Date', $opening_Date);
+        $stm->bindParam(':Conclusion_Date', $conclusion_Date);
+        $stm->bindParam(':status', $status);
+        $stm->execute();
+        header("location: add_class.php");
+    }
 }
 ?>
